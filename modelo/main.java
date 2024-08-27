e45dca6f53b9426c7b6a2f8ac1355e8c43c0c918
abelardo <abelardoingaataucuri271@gmail.com>
2024-08-27 15:44:34 -0500

    mi trabajo persona

---
+++ modelo/main.java
@@ -0,0 +1,13 @@
+package modelo;
+
+public class main {
+    public static void main(String[] args) {
+        // Crear una instancia de Persona
+        Persona persona = new Persona(10L, "ABELARDO", "EULOGIO", "INGA ATAUCURI", "940271923");
+
+        // Mostrar la información de la persona
+        persona.mostrarInformacion();
+
+    }
+}
+
